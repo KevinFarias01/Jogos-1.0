af6f7c135dfd280383d9d46c318d4096cc9527c3
Kevin Farias <kevin.farias.rib@gmail.com>
2021-08-16 12:45:25 -0300

    3 carros

--- Jogo do carrinho/material/sketch.js
+++ Jogo do carrinho/material/sketch.js
@@ -1,44 +1,12 @@
-let imagemDaEstrada;
-let imagemDoAtor;
-let imagemCarro;
-
-let xCarro= 600
-let yAtor= 366
-function preload(){
-    imagemDaEstrada = loadImage("img/estrada.png");
-    imagemDoAtor = loadImage("img/ator-1.png");
-    imagemCarro = loadImage("img/carro-1.png")
-}
-
 function setup() {
-    createCanvas(500, 400);
+  createCanvas(500, 400);
 }
 
 function draw() {
-    background(imagemDaEstrada);
-    mostraAtor();
-    mostraCarro();
-    movimentaCarro();
-    movimentaAtor();
-}
-
-function mostraAtor(){
-  image(imagemDoAtor, 100, yAtor, 30, 30); //posx,posy,alt,compri
-}
-
-function mostraCarro(){
-  image(imagemCarro, xCarro, 40, 50, 40)
-}
-
-function movimentaCarro(){
-  xCarro-=2;
-}
-
-function movimentaAtor(){
-  if (keyIsDown(UP_ARROW)){
-    yAtor -= 3;
-  }
-  if (keyIsDown(DOWN_ARROW)){
-    yAtor += 3;
-  }
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
 }
